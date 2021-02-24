e233e0df79e5dc1dd17b4fedc55e23f3a8837ea0
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2021-02-24 21:20:19 +0900

    2021/2/24_演習1-3_1回目_進捗率100％

---
+++ sql_drill/src/sql01_03/01_03_02.sql
@@ -0,0 +1,35 @@
+■問題
+アクセス記録テーブル（access_log）からリンク元URLを重複のない形式で取り出してみましょう。以下の空欄を埋めてSQL命令を完成させてください。
+[①空欄]
+	referer
+[②空欄]
+	access_log
+;
+
+■実行文
+
+SELECT DISTINCT
+	referer
+FROM
+	access_log
+;
+
+
+■返却値
+mysql> SELECT DISTINCT
+    -> referer
+    -> FROM
+    -> access_log
+    -> ;
++---------------------------+
+| referer                   |
++---------------------------+
+| http://wings.msn.to/hamu/ |
+| http://wings.msn.to/neko/ |
+| http://wings.msn.to/inu/  |
+| http://wings.msn.to/saru/ |
+| http://wings.msn.to/tori/ |
+| http://wings.msn.to/kame/ |
+| NULL                      |
++---------------------------+
+7 rows in set (0.01 sec)
